04f34654ee0a669c61c5a74b9a4df024ad456130
NATHAN HOCHSTRASSER <nathanhochstrasser@MacBook-Pro-de-NATHAN.local>
2020-03-04 12:08:48 +0100

    LEMP + Wordpress

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,35 @@
 FROM debian:buster
 
-RUN apt-get -y update \
-&& apt-get -y upgrade
+RUN apt-get -y update
+RUN apt-get -y upgrade
 
-RUN apt-get -y install nginx
+RUN apt-get -y install nginx \
+&& apt-get -y install default-mysql-server \
+&& apt-get -y install php php-fpm php-mysql php7.3-gd php-ssh2 \
+&& apt-get -y install wget \
+&& wget http://wordpress.org/latest.tar.gz \
+&& tar xzvf latest.tar.gz
 
-WORKDIR /var/www/
+ADD ./srcs/info.php /var/www/html/
+ADD ./srcs/wordpress /etc/nginx/sites-available/
+ADD ./srcs/wp-config.php /wordpress
+ADD /srcs/db.sql /tmp/
+
+RUN service mysql start \
+&& service php7.3-fpm start \
+&& service nginx start \
+	&& mysql -u root < /tmp/db.sql \
+	&& rsync -avP /wordpress/ /var/www/html/ \
+	&& chown -R root:www-data /var/www/html/* \
+	&& mkdir /var/www/html/wp-content/uploads \
+	&& chown -R root:www-data /var/www/html/wp-content/uploads \
+	&& ln -s /etc/nginx/sites-available/wordpress /etc/nginx/sites-enabled/ \
+	&& rm /etc/nginx/sites-enabled/default \
+&& service php7.3-fpm restart\
+&& service nginx restart
 
 EXPOSE 80
 
-RUN service nginx start
-CMD nginx -g "daemon off;"
+# CMD service mysql start \
+# && service php7.3-fpm restart \
+# && nginx -g "daemon off;"
---
+++ srcs/wp-config.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'wordpress' );
+
+/** MySQL database username */
+define( 'DB_USER', 'wordpressuser' );
+
+/** MySQL database password */
+define( 'DB_PASSWORD', 'password' );
+
+/** MySQL hostname */
+define( 'DB_HOST', 'localhost' );
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         ' T1;lu%_}X@;,8?{^r!vLKC<MA55?B5wN=b^0JA`*HIG&1<(=@yz>lo}twK.:v$3');
+define('SECURE_AUTH_KEY',  '>!_{(_T.5,vndwT/K6}~0^PHq?Z}6vzN1+JKDiTj6Zp?zW#v5)Bj+i0+[+!)opw|');
+define('LOGGED_IN_KEY',    '+5tu>o@|Y6DP/I2%9<ST+2[Hc&#i^]ke1wh)0g1K_-PSs6JFmm)j:Jm5w{0<eqU ');
+define('NONCE_KEY',        'G8P-W@sTy|%Zw6FTNN4*~iAxp(sVdoA+~$[-F_@)49CGS$ZC9</>+7{>+E5n^X5w');
+define('AUTH_SALT',        'OJ?1%B`@(R5&A3|CH;xSZv|!45P|^)f#9$n.a0Fd5EUsd>L9fyP7=~]HFu9T_Y<{');
+define('SECURE_AUTH_SALT', 's)uU8mv|o1glpggZ?FKW!9w3E04(z-mp0uXdOnu(`+v,iTIov,c.}9UaPJOX?b}8');
+define('LOGGED_IN_SALT',   '4f7!7n?R@=DOa& 4xOgAy[-^>C{V9v/kn?+&yJphpc7)|DMMo8HlYL/O4_jNb;VQ');
+define('NONCE_SALT',       'w*StYo@.7zX/-C}Akk<T[DIuv^LaHn,U3/WS;XUb10.1>;T;Bt9/P2rTuU|9sA~G');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define( 'WP_DEBUG', false );
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once( ABSPATH . 'wp-settings.php' );
